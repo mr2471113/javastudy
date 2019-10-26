3311b3e710da67d5ab2399aaad2597ea514d4ce0
it <it@DESKTOP-KKAQ0RK>
2019-10-26 16:33:44 +0900

    19.10.26 최종 업로드

---
+++ javaStudy/src/file/File2.java
@@ -0,0 +1,22 @@
+package file;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class File2 {
+
+	public static void main(String[] args) {
+		try {
+			FileInputStream fo = new FileInputStream("C:/eclipse/file.txt");
+			int data = fo.read();
+			System.out.println(data);
+			System.out.println((char)data);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
